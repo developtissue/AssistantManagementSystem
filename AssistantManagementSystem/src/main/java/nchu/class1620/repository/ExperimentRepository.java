4ee678b9844e4349212772c91df1d171f4d91bde
S16202130 <768955624@qq.com>
2019-06-16 13:15:25 +0800

    zhang branch update

--- AssistantManagementSystem/src/main/java/nchu/class1620/repository/ExperimentRepository.java
+++ AssistantManagementSystem/src/main/java/nchu/class1620/repository/ExperimentRepository.java
@@ -1,5 +1,6 @@
 package nchu.class1620.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -7,6 +8,7 @@ import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import nchu.class1620.entity.Experiment;
+import nchu.class1620.entity.Report;
 
 @Mapper
 public interface ExperimentRepository {
@@ -20,10 +22,19 @@ public interface ExperimentRepository {
 	 * want.
 	 */
 	@Select("select e.e_id,e.e_name,e.e_content from assistadmin.experiment e,assistadmin.markexperimentreport me where me.e_id=e.e_id and me.assist_id=#{assit_id}")
-	@Results({ @Result(column = "e.e_id", property = "e_id"), @Result(column = "e.e_name", property = "e_name"),
-			@Result(column = "e.e_content", property = "e_content") })
+	@Results({ @Result(column = "e_id", property = "e_id",javaType = Integer.class), @Result(column = "e_name", property = "e_name", javaType = String.class),
+			@Result(column = "e_content", property = "e_content", javaType = String.class) })
 	public List<Experiment> findByAssitantId(int assit_id);
-	
-	@Select("SELECT e_name from experiment where e_id=#{eid}")
+
+	@Select("SELECT e_name from assistadmin.experiment where e_id=#{eid}")
 	public String findByExperimentName(int e_id);
+
+	@Select("select s.s_id,s.s_name,e_contnt,me.mer_grade,me.comment\r\n"
+			+ "from assistadmin.student s,assistadmin.markexperimentreport me,assistadmin.experiment e\r\n"
+			+ "where s.s_id=me.s_id and me.e_id=e.e_id and  s.cls_id=#{class_id}")
+	@Results({ @Result(column = "s.s_id", property = "s_id"), @Result(column = "s.s_name", property = "s.s_name"),
+			@Result(column = "e_contnt", property = "title"), @Result(column = "me.mer_grade", property = "socre"),
+			@Result(column = "me.comment", property = "comment") })
+	public ArrayList<Report> findExperiemntRportByClass(int class_id);
+
 }
