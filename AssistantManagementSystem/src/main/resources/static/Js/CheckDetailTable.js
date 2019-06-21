66ac214b4a3effb37f3acb8c4b00e2f15e8e935c
S16202128 <1304542866@qq.com>
2019-06-21 10:25:29 +0800

    update

--- AssistantManagementSystem/src/main/resources/static/Js/CheckDetailTable.js
+++ AssistantManagementSystem/src/main/resources/static/Js/CheckDetailTable.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {	
 	
+	if ($('#status').val() == '录取') {
+		$('#accepts').hide();
+		$('#refuse').hide();
+		$('#comment').attr("disabled",true);
+	}
+	$('#accepts').click(function(){
+		var temp = $('#comment').val();
+		var temp1 = $('#first-name').val();
+		var urls = "http://localhost:8080/ModifyComment/"+"1/"+ temp1 +"/"+temp;			
+		
+		$.ajax({
+			url: urls,
+	       	type: 'POST'
+	    }).then(function(data) {
+	    	alert("修改成功！");
+	    	window.location.href = "http://localhost:8080/CheckApplyTable";
+	    });
+		
+	});
 	$('#refuse').click(function(){
 		var temp = $('#comment').val();
 		var temp1 = $('#first-name').val();
