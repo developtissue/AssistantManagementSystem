faaec3d5bd8b36f09943b51ce7dfead9deaf34f0
S16202128 <1304542866@qq.com>
2019-06-29 17:51:13 +0800

    yu

--- AssistantManagementSystem/src/test/java/nchu/class1620/AssitantManagementSystemApplicationTests.java
+++ AssistantManagementSystem/src/test/java/nchu/class1620/AssitantManagementSystemApplicationTests.java
@@ -9,10 +9,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import nchu.class1620.AssitantManagementSystemApplication;
 import nchu.class1620.entity.AttendExperiment;
+import nchu.class1620.entity.Teacher;
 import nchu.class1620.dto.MyEntity1;
 import nchu.class1620.repository.AttendExperimentRepository;
 import nchu.class1620.repository.TeacherRepository;
+import nchu.class1620.service.AdminService;
+import nchu.class1620.service.AssistantService;
 import nchu.class1620.service.MyService1;
+import nchu.class1620.service.StudentService;
 import nchu.class1620.service.TeacherService;
 
 @RunWith(SpringRunner.class)
@@ -21,27 +25,23 @@ import nchu.class1620.service.TeacherService;
 public class AssitantManagementSystemApplicationTests {
 
 	@Autowired
-	TeacherService teacherService;
+	TeacherService ts;
 	
 	@Autowired
-	TeacherRepository repo;
+	AdminService as;
+	
+	@Autowired
+	StudentService ss;
+	
+	@Autowired
+	AssistantService ass;
 	
 	@Test
 	public void test() {
-		Integer result = repo.FindCourseId("1sfd");
-		System.out.println(result);
+		Teacher t = new Teacher(); 
+		t.setId(1);
+		t.setName("张恒峰");
+		t.setPassword("123");
+		System.out.println(ts.InsertPublishRecruitment(t, "web", "需要努力的人"));
 	}
-	
-//	@Test
-//	public void test() {
-//		List<AttendExperiment> list = repo.findByStuId(/* 1620**** */);
-//		System.out.println(list);
-//	}
-//	
-//	@Test
-//	public void testService() {
-//		List<MyEntity1> list = service.getMyEntity1List(/* 1620**** */);
-//		System.out.println(list);
-//	}
-
 }
